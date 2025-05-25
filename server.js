e283c80650cb9552e9191107279fcef3b5ca8916
Shoham Shtiler <shtilershoham@gmail.com>
2025-05-25 20:46:13 +0300

    Fix: added production frontend domain to CORS whitelist

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('public'))
 } else {
 	const corsOptions = {
-		origin: ['http://127.0.0.1:3000', 'http://localhost:3000', 'http://localhost:5173', 'http://127.0.0.1:5173'],
+		origin: ['https://mistertoy-backend-2oeq.onrender.com','http://127.0.0.1:3000', 'http://localhost:3000', 'http://localhost:5173', 'http://127.0.0.1:5173'],
 		credentials: true,
 	}
 	app.use(cors(corsOptions))
